b2e00a2596b9641a83eafa016d66a880bfba8a90
rodocastellanos <rocascas81@hotmail.com>
2022-11-06 11:44:36 -0300

    06_11_2022_11_45

--- src/components/CarruselIngreso.jsx
+++ src/components/CarruselIngreso.jsx
@@ -47,7 +47,6 @@ function CarruselIngreso(props){
                         <div class="mb-3">
                           <label for="exampleInputPassword1" class="form-label font-weight-bold">Contraseña</label>
                           <input type="password" class="form-control border-0" placeholder="Ingresa tu contraseña" id="exampleInputPassword1" required></input>
-                          <input type="text" class="form-control border-0" placeholder="Ingresa tu usario" id="exampleInputEmail1" aria-describedby="emailHelp" required></input>
                         </div>
                         <div id="errorBox">
                         </div>
