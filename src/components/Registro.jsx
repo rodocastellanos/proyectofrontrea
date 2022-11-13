a1e3ac7bd283d7a38e14bde8995762b4538a3954
sebastamayo7 <sebastamayo7@gmail.com>
2022-11-13 09:13:07 -0500

    CambiosFrontEnd

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,19 +1,48 @@
-import './styles/registrostyle.css'
+import "./styles/registrostyle.css";
 
-function Registro(){
+function Registro() {
     return (
-    <div className="form-register">
+        <div className="form-register">
             <h4>Formulario Registro</h4>
             <form method="post">
-                <input className="controls" type="text" name="nombres" id="nombres" placeholder="Ingresa tu nombre"></input>
-                <input className="controls" type="text" name="username" id="username" placeholder="Ingresa un username"></input>
-                <input className="controls" type="email" name="correo" id="correo" placeholder="Ingresa un correo"></input>
-                <input className="controls" type="password" name="contraseña" id="contraseña" placeholder="Ingresa una contraseña"></input>
+                <input
+                    className="controls"
+                    type="text"
+                    name="nombres"
+                    id="nombres"
+                    placeholder="Ingresa tu nombre"
+                ></input>
+                <input
+                    className="controls"
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Ingresa un username"
+                ></input>
+                <input
+                    className="controls"
+                    type="email"
+                    name="correo"
+                    id="correo"
+                    placeholder="Ingresa un correo"
+                ></input>
+                <input
+                    className="controls"
+                    type="password"
+                    name="contraseña"
+                    id="contraseña"
+                    placeholder="Ingresa una contraseña"
+                ></input>
                 <div id="errorBox"></div>
-                <input className="botons" type="button" onclick="register()" value="Registrarse"></input>
+                <input
+                    className="botons"
+                    type="button"
+                    onclick="register()"
+                    value="Registrarse"
+                ></input>
             </form>
-    </div>
-    )
+        </div>
+    );
 }
 
 export default Registro;
