054f8326a35de134b64f569c8e3d171268dd4734
Johan Robles <johanrobles66@yahoo.com>
2022-11-26 15:52:45 -0500

    Apuntando al backend de la nube

--- src/config.js
+++ src/config.js
@@ -1,9 +1,21 @@
-let PORT = 8080;
-let HOST = "localhost";
-let PROTOCOL = "http";
+// let PORT = 8080;
+// let HOST = "localhost";
+// let PROTOCOL = "http";
+// let API_ROUTE = "/api/";
+// let FULL_PATH = PROTOCOL + "://" + HOST + ":" + PORT + "/";
+// let FULL_API_PATH = PROTOCOL + "://" + HOST + ":" + PORT + API_ROUTE;
+
+let PORT = 80;
+let HOST = "backend-proyecto-campesinos.vercel.app";
+let PROTOCOL = "https";
 let API_ROUTE = "/api/";
 let FULL_PATH = PROTOCOL + "://" + HOST + ":" + PORT + "/";
 let FULL_API_PATH = PROTOCOL + "://" + HOST + ":" + PORT + API_ROUTE;
+
+if (PORT === 80) {
+    FULL_PATH = PROTOCOL + "://" + HOST + "/";
+    FULL_API_PATH = PROTOCOL + "://" + HOST + API_ROUTE;
+}
 
 module.exports = {
     PORT,
